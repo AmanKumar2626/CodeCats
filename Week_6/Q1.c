64eb2797f0ff477a246b455bdacbb5b7b9635907
AMAN KUMAR <92772172+AmanKumar2626@users.noreply.github.com>
2021-12-05 02:04:25 +0530

    Update Q1.c

--- Week_6/Q1.c
+++ Week_6/Q1.c
@@ -1,4 +1,4 @@
-//1. Find one extra character in a string 
+//1. Find one extra character in a string.
 #include<stdio.h>
 #include<string.h>
 int main()
@@ -6,9 +6,9 @@ int main()
 	int l1,l2,i,j;
 	char str1[20],str2[20];
 	printf("string strA = ");
-    scanf("%s",str1);
+        scanf("%s",str1);
 	printf("string strB = ");
-    scanf("%s",str2);
+        scanf("%s",str2);
 	l1=strlen(str1);
 	l2=strlen(str2);
 	for(i=0;i<l2;i++)
